e35aae017eedca98d9a649fedfa539c0b219f40a
rishadpt <rishadpt16@gmail.com>
2022-07-21 15:53:15 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Mainform from "./components/Mainform/Mainform";
+import './App.scss'
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import Second from "./components/Second/Second";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <BrowserRouter>
+        <Routes>
+          <Route  path="/" element={<Mainform />}/>
+          <Route path="/second" element={<Second />}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
